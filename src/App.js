4ff013830b230f96d2a877783e344f0aa974fb55
Diego Alvarez <doctordalvarez@protonmail.com>
2021-08-03 09:46:31 -0300

    login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 import React from 'react'
-import MainTabs from './Tabs';
+import { Tabs, 
+  TabList, 
+  Tab, 
+  TabPanels, 
+  TabPanel 
+} from "@reach/tabs"
+
 
 function App() {
   return (
-    <div className="App">
-      <MainTabs />
+    <div className="login">
+    <Tabs>
+      <TabList>
+        <Tab>Login</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <form className="login-form">
+            <label htmlFor="username"></label>
+            <input type="email" id="username" placeholder="Email" autoComplete="off" />
+            <br></br>
+            <label htmlFor="password"></label>
+            <input type="password" id="password" placeholder="Password"/>
+          </form>
+        </TabPanel>
+        <TabPanel>
+          
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
     </div>
   );
 }
